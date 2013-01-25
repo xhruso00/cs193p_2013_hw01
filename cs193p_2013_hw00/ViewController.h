85aa4ec36f83c894437ddb7dbbae1b72a5f68610
Marek Hrušovský <xhruso00@Mareks-MacBook-Pro.local>
2013-01-25 17:31:45 +0100

    Initial Commit

---
+++ cs193p_2013_hw00/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  cs193p_2013_hw00
+//
+//  Created by Marek Hrušovský on 1/25/13.
+//  Copyright (c) 2013 Marek Hrušovský. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ cs193p_2013_hw00/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  cs193p_2013_hw00
+//
+//  Created by Marek Hrušovský on 1/25/13.
+//  Copyright (c) 2013 Marek Hrušovský. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
